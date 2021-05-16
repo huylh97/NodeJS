60d66aca7c5426b9027cc9e19216af84f646f686
lehoanghuy <huylh@weits-Mac-mini.local>
2021-05-16 16:30:28 +0700

    nodejs api project

---
+++ sever-nodejs-firebase/models/student.js
@@ -0,0 +1,12 @@
+class Student {
+    constructor(id, firstName, lastName,
+        age, phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+}
+
+module.exports = Student;
